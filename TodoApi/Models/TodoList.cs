98e986a296336c972f409aad81cee067873f9165
Emiliano Marotta <emilianomarott@gmail.com>
2025-06-07 15:25:44 -0300

    add item List in TodoList

--- TodoApi/Models/TodoList.cs
+++ TodoApi/Models/TodoList.cs
@@ -4,4 +4,11 @@ public class TodoList
 {
     public long Id { get; set; }
     public required string Name { get; set; }
+    public ICollection<Item> Items { get; set; } = new List<Item>();
+
+    public override bool Equals(object? obj)
+    {
+        TodoList? todoList = (TodoList)obj;
+        return Id == todoList.Id || Name == todoList.Name;
+    }
 }
