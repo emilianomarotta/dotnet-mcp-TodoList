ff8c77a872c941ca8eb22caef6abf98de288ab8e
Emiliano Marotta <emilianomarott@gmail.com>
2025-06-07 15:26:18 -0300

    add CreateItem model

---
+++ TodoApi/Dtos/CreateItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Dtos
+{
+    public class CreateItem
+    {
+        public required string TodoListName { get; set; }
+        public required string Description { get; set; }
+
+
+
+    }
+}
